de3df7dde8f3216cb0c2dc0eb7f48ed51bbe4a71
Krayem12 <93749589+Krayem12@users.noreply.github.com>
2025-12-22 22:12:31 +0300

    Update init.py

--- core/init.py
+++ core/init.py
@@ -1,1 +1,21 @@
-# Empty init file
+"""
+📦 حزمة النظام الأساسي للتداول
+"""
+
+__version__ = "1.0.0"
+__author__ = "Trading System Team"
+
+# تصدير الفئات الرئيسية
+from .trade_manager import TradeManager
+from .group_manager import GroupManager
+from .signal_processor import SignalProcessor
+from .webhook_handler import WebhookHandler
+from .redis_manager import RedisManager
+
+__all__ = [
+    'TradeManager',
+    'GroupManager',
+    'SignalProcessor',
+    'WebhookHandler',
+    'RedisManager'
+]
